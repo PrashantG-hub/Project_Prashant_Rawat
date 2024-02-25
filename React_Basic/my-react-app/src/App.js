f88ede84354c6b4341c750f5156021686346dfb4
PrashantG-hub <125962967+PrashantG-hub@users.noreply.github.com>
2024-02-25 17:29:43 +0530

    react_task1

---
+++ React_Basic/my-react-app/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Header from './components/header';
+// import Videocard from './components/Videocard';
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      {/* <Videocard /> */}
+    </div>
+  );
+}
+
+
+export default App;
